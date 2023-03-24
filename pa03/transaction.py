e1c006154c308b7d655670f4663be2714760ff95
areenkh <areen.khalaila@gmail.com>
2023-03-24 15:28:01 -0400

    added modify transaction method

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -17,18 +17,18 @@ class Transaction:
     #    return self.runQuery("UPDATE transaction SET completed=1 WHERE rowid=(?)",(rowid,))
     #should we set the date of transaction to Datetime.now()
 
-    '''
-    def add(self,item):
-        #areen
-        ''' create a transaction item and add it to the transaction table '''
-        return self.runQuery("INSERT INTO transaction VALUES(?,?,?)",(item['amount'],description['desc'],item['date']))
-
-    def delete(self,rowid):
-        #areen
-        ''' delete a transaction item '''
-        return self.runQuery("DELETE FROM transaction WHERE itemid=(?)",(itemid,))
-    '''
     
+    #def add(self,item):
+    #    #areen
+    #    ''' create a transaction item and add it to the transaction table '''
+    #    return self.runQuery("INSERT INTO transaction VALUES(?,?,?)",(item['amount'],description['desc'],item['date']))
+
+    #def delete(self,rowid):
+    #    #areen
+    #    ''' delete a transaction item '''
+    #    return self.runQuery("DELETE FROM transaction WHERE itemid=(?)",(itemid,))
+    
+
     def add_transaction(self, itemid, amount, category, date, description):
         #areen
         self.run_query('''
@@ -39,6 +39,26 @@ class Transaction:
     def delete_transaction(self, transaction_id):
         #areen
         self.run_query('DELETE FROM transaction WHERE itemid = ?', (itemid,))
+
+    def modify_transaction(self, itemif, amount=None, category=None, date=None, description=None):
+        update_query = 'UPDATE transaction SET '
+        update_args = []
+        if amount is not None:
+            update_query += 'amount = ?, '
+            update_args.append(amount)
+        if category is not None:
+            update_query += 'category = ?, '
+            update_args.append(category)
+        if date is not None:
+            update_query += 'date = ?, '
+            update_args.append(date)
+        if description is not None:
+            update_query += 'description = ?, '
+            update_args.append(description)
+        update_query = update_query.rstrip(', ')
+        update_query += ' WHERE itemid = ?'
+        update_args.append(itemid)
+        self.run_query(update_query, tuple(update_args))
     
     def runQuery(self,query,tuple):
         ''' return all of the uncompleted tasks as a list of dicts.'''
