57de1b43bb3bd136b55a71a515640fd69b8d0821
areenkh <areen.khalaila@gmail.com>
2023-03-24 13:48:40 -0400

    added method to transaction file

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,3 +1,9 @@
+def toDict(t):
+    ''' t is a tuple ('item id','amount','category','date','description')'''
+    print('t='+str(t))
+    transaction = {'itemid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
+    return transaction
+
 class Transaction:
     def __init__(self, filename):
         self.cnct = sqlite3.connect(filename)
