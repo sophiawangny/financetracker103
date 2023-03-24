536e0ecbab7074a8953217b67a4f11db7a2ad2f3
areenkh <areen.khalaila@gmail.com>
2023-03-24 13:54:06 -0400

    added runQuery method

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -8,3 +8,21 @@ def toDict(t):
 class Transaction:
     def __init__(self, filename):
         self.cnct = sqlite3.connect(filename)
+
+    #suggester __init__(self, filename) fro lecture 19 like the professor asked in mastery
+    #def __init__(self):
+    #   self.runQuery('''CREATE TABLE IF NOT EXISTS transaction
+    #               (amount text, category text, date text, description text)''',())
+    #amount might be int
+    #    return self.runQuery("UPDATE transaction SET completed=1 WHERE rowid=(?)",(rowid,))
+    #should we set the date of transaction to Datetime.now()
+    
+    def runQuery(self,query,tuple):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        con= sqlite3.connect(os.getenv('HOME')+'/transaction.db')
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
