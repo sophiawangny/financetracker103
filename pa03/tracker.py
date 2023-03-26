32d66e77fcc6d659851d7061e09321c0a2d8c39a
Yalda Mauj <64442722+yaldamauj@users.noreply.github.com>
2023-03-26 08:47:45 -0400

    added some tests, editted the programs to pass as many pylint tests

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -1,8 +1,9 @@
-# offers the user the following options and makes calls to the Transaction class to update the database.
+"""offers the user the following options 
+and makes calls to the Transaction class to update the database"""
 
 from transaction import Transaction
 
-menu_options = '''Please select the number corresponding the command:
+MENU_OPTIONS = '''Please select the number corresponding the command:
 0. quit
 1. show categories
 2. add category
@@ -17,102 +18,80 @@ menu_options = '''Please select the number corresponding the command:
 11. print this menu
 '''
 
+"""This method process the user input"""
 def process_choice(choice):
-	transaction = Transaction() # create an instance of the Transaction class
-
-
+    transaction=Transaction()
     if choice=='0': #quit
         return
-
-    elif choice=='1': #show categories
-        ctgries = transaction.select_all() 	 #calls the select_all method from Transaction 
-        print("Categories:")
+    if choice=='1': #show categories
+        ctgries = transaction.select_all()
         for cat in ctgries:
             print(f"- {cat}")
-
     elif choice=='2': #add category
         name = input("name of category: ")
         desc = input("category description: ")
         ctgry = {'name':name, 'desc':desc}
-        transaction.add_category(ctgry) 			#calls the add_Category method from Transaction
-
+        transaction.add_category(ctgry)
     elif choice=='3': #modify category
         print("Modify Category:")
-        id = int(input("id: "))
+        id_num = int(input("id: "))
         name = input("new name of category: ")
         desc = input("new category description: ")
         ctgry = {'name':name, 'desc':desc}
-        transaction.update_category(id,ctgry)		#calls the update_category method from Transaction 
-
-    elif choice == '4': #show transactions 
-        transactions = transaction.get_transactions() 	#calls the get_transactions method from Transaction
+        transaction.update_category(id_num,ctgry)
+    elif choice == '4': #show transactions
+        transactions = transaction.get_transactions()
         print("Transactions:")
-        for t in transactions:
-            print(f"- {t['itemid']}: {t['amount']} - {t['category']} - {t['date']} - {t['description']}")
-
-    elif choice == '5': #add transaction 
+        for trans in transactions:
+            print(f"- {trans['itemid']}: {trans['amount']} "
+                    f"- {trans['category']} - {trans['date']} "
+                    f"- {trans['description']}")
+    elif choice == '5': #add transaction
         itemid = input("itemid: ")
         amount = input("Enter the amount: ")
         ctgry = input("Enter the category: ")
         date = input("Enter the date (YYYY-MM-DD): ")
         description = input("Enter the transaction description: ")
-        transaction.add_transaction(itemid, amount, ctgry, date, description)  #calls the add_transaction method from Transaction 
-
+        transaction.add_transaction(itemid, amount, ctgry, date, description)
     elif choice == '6': #delete transaction
         item_num = input("Enter the item number to delete: ")
-        transaction.delete_transaction(item_num)		#calls the delete_transaction method from Transaction
-
-    elif choice=='7':      # summarize transactions by date
+        transaction.delete_transaction(item_num)
+    elif choice=='7': #summarize transactions by date
         date=input("Enter the date (MM-DD-YYYY):")
-        summary = transaction.summarize_transactions_by_date(date) #Calls the summarize_transaction_by_date method from Transaction class 
+        summary = transaction.get_transactions_by_date(date)
         print(f"Transactions on {date}:")
         for ctgry, amount in summary.items():
             print(f"- {ctgry}: {amount}")
-
-    elif choice == '8': # summarize transactions by month (MM)
-        month = input("Enter the month (YYYY-MM): ")
-        summary = transaction.summarize_transactions_by_month(month) #Calls the summarize_transaction_by_month
+    elif choice == '8': #summarize transactions by month (MM)
+        month = input("Enter the month (MM): ")
+        summary = transaction.summarize_transactions_by_month(month)
         print(f"Transactions in {month}:")
         for ctgry, amount in summary.items():
             print(f"- {ctgry}: {amount}")
-
     elif choice == '9': #summarize transactions by year (YYYY)
         year = input("Enter the year (YYYY): ")
-        summary = transaction.summarize_transactions_by_year(year) #Calls the summarize_transaction_by_year
+        summary = transaction.summarize_transactions_by_year(year)
         print(f"Transactions in {year}:")
         for ctgry, amount in summary.items():
             print(f"- {ctgry}: {amount}")
-
     elif choice == '10': #summarize transactions by category
         ctgry = input("Enter the category: ")
-        summary = transaction.summarize_transactions_by_category(ctgry) 
+        summary = transaction.get_transactions_by_category(ctgry)
         print(f"Transactions in '{ctgry}':")
         for date, amount in summary.items():
             print(f"- {date}: {amount}")
-
     elif choice=='11': #print this menu
-        print(menu_options)
-
+        print(MENU_OPTIONS)
     else:
         print("Invalid choice, try again")
 
-
-
 def toplevel():
     ''' take in user choice '''
-    print(menu_options)
+    print(MENU_OPTIONS)
     choice = input("> ")
     while choice !='0' :
-        choice = process_choice(choice)
+        process_choice(choice)
     print('Thanks!')
 
-
 toplevel()
-
-
-    
-
-
-
-
         
--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,124 +1,103 @@
-import sqlite3
-import os
 
-def toDict(t):
+"""transaction.py is a Object Relational Mapping to the transactions table
+    This app will store the data in a SQLite database ~/trans.db
+"""
+import sqlite3
+
+def to_dict(trans):
     #areen
     ''' t is a tuple ('item id','amount','category','date','description')'''
-    print('t='+str(t))
-    transaction = {'itemid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
-    return transaction
+    print('transaction='+str(trans))
+    result = {'itemid':trans[0], 'amount':trans[1],
+    'category':trans[2], 'date':trans[3], 'description':trans[4]}
+    return result
 
 
-###Yalda 
 def transaction_list(transaction_tuples):
-    return [toDict(transaction) for transaction in transaction_tuples]
+    #Yalda
+    ''' convert a list of transaction tuples into a list of dictionaries'''
+    return [to_dict(transaction) for transaction in transaction_tuples]
 
 class Transaction:
-
-   def __init__(self):
-        self.db = sqlite3.connect('trans.db')
-        self.runQuery('''CREATE TABLE IF NOT EXISTS transaction
+    ''' Transaction represents a table of transaction'''
+    def __init__(self):
+        self.database = sqlite3.connect('trans.db')
+        self.run_query('''CREATE TABLE IF NOT EXISTS transaction
                           (itemid real, amount real, category text, date text, description text)''')
-
-
-    
-
-    #def add(self,item):
-    #    #areen
-    #    ''' create a transaction item and add it to the transaction table '''
-    #    return self.runQuery("INSERT INTO transaction VALUES(?,?,?)",(item['amount'],description['desc'],item['date']))
-
-    #def delete(self,rowid):
-    #    #areen
-    #    ''' delete a transaction item '''
-    #    return self.runQuery("DELETE FROM transaction WHERE itemid=(?)",(itemid,))
-    
 
     def add_transaction(self, itemid, amount, category, date, description):
         #areen
-        self.runQuery('''INSERT INTO transaction (itemid, amount, category, date, description)
-                         VALUES (?, ?, ?, ?, ?)''', (itemid, amount, category, date, description))
-
+        ''' add a transaction to the transactions table.'''
+        self.run_query('''
+            INSERT INTO transaction 
+            (itemid, amount, category, date, description) 
+            VALUES (?, ?, ?, ?, ?)
+        ''', (itemid, amount, category, date, description))
 
     def delete_transaction(self, itemid):
         #areen
-        self.runQuery('''DELETE FROM transaction WHERE itemid = ?''', (itemid,))
+        ''' delete a transaction with a specified item id'''
+        self.run_query('''DELETE FROM transaction WHERE itemid = ?''', (itemid,))
 
-
-    ###Yalda
     def get_transactions(self):
-        cursor = self.db.cursor()
+        #Yalda
+        '''returns a list of transactions'''
+        cursor = self.database.cursor()
         cursor.execute('''SELECT * FROM transaction''')
         transactions = cursor.fetchall()
-        return [toDict(t) for t in transactions]
-
+        return [to_dict(t) for t in transactions]
 
     def get_transactions_by_date(self, date):
         #areen
-        return self.db.execute('''SELECT * FROM transaction WHERE date = ?''', (date,)).fetchall()
-    
+        '''returns a list of transactions by date'''
+        query = '''SELECT * FROM transaction WHERE date = ?'''
+        return self.database.execute(query, (date,)).fetchall()
 
     def get_transactions_by_category(self, category):
         #areen
-        return self.db.execute('''SELECT * FROM transaction WHERE category = ?''', (category,)).fetchall()
+        '''returns a list of transactions by category'''
+        return self.database.execute('''
+            SELECT * 
+            FROM transaction 
+            WHERE category = ?
+            ''', (category,)).fetchall()
 
 
-    ######we dont need this method?
-    # def modify_transaction(self, itemif, amount=None, category=None, date=None, description=None):
-    #     #areen
-    #     update_query = 'UPDATE transaction SET '
-    #     update_args = []
-    #     if amount is not None:
-    #         update_query += 'amount = ?, '
-    #         update_args.append(amount)
-    #     if category is not None:
-    #         update_query += 'category = ?, '
-    #         update_args.append(category)
-    #     if date is not None:
-    #         update_query += 'date = ?, '
-    #         update_args.append(date)
-    #     if description is not None:
-    #         update_query += 'description = ?, '
-    #         update_args.append(description)
-    #     update_query = update_query.rstrip(', ')
-    #     update_query += ' WHERE itemid = ?'
-    #     update_args.append(itemid)
-    #     self.runQuery(update_query, tuple(update_args))
-
-
-
-     ###Yalda 
     def select_all(self):
-        cursor = self.db.cursor()
+        #Yalda
+        ''' return all of the transactions as a list of dicts.'''
+        cursor = self.database.cursor()
         cursor.execute('''SELECT name FROM categories''')
         return cursor.fetchall()
 
+    def add_category(self, category):
+        #Yalda
+        ''' add a category to the table.'''
+        cursor = self.database.cursor()
+        query = '''INSERT INTO categories(name, description) VALUES(?, ?)'''
+        values = (category['name'], category['desc'])
+        cursor.execute(query, values)
+        self.database.commit()
 
-    ###Yalda
-	def add_category(self, category):
-    	cursor = self.db.cursor()
-    	cursor.execute('''INSERT INTO categories(name, description) VALUES(?, ?)''', (category['name'], category['desc']))
-    	self.db.commit()
+    def update_category(self, id_num, category):
+        #Yalda
+        '''modify a category in the table.'''
+        cursor = self.database.cursor()
+        query = '''UPDATE categories SET name = ?, description = ? WHERE id_num = ?'''
+        values = (category['name'], category['desc'], id_num)
+        cursor.execute(query, values)
+        self.database.commit()
 
- 
-
-    ###Yalda
- 	def update_category(self, id, category):
-    	cursor = self.db.cursor()
-    	cursor.execute('''UPDATE categories SET name = ?, description = ? WHERE id = ?''', (category['name'], category['desc'], id))
-    	self.db.commit()
-
-
-    
-    def runQuery(self, query, params=None):
-        # execute a SQL query on the database
+    def run_query(self, query, params=None):
+        '''execute a SQL query on the database'''
         if params is None:
             params = []
-        cursor = self.db.cursor()
+        cursor = self.database.cursor()
         cursor.execute(query, params)
-        self.db.commit()
+        self.database.commit()
         return cursor.fetchall()
 
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     transaction=Transaction()
